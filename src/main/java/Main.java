3d8683352fe22ce446f703e0990ad7945873e80e
brunovsilv <169068441+brunovsilv@users.noreply.github.com>
2024-05-13 23:16:39 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Série favorita: La casa de Papel");
+    System.out.println("");
+    System.out.println("Sinopse: Oito ladrões fazem reféns e fecham-se dentro da Casa Nacional da Moeda de Espanha, enquanto um génio do crime manipula a polícia para executar o seu plano. Veja tudo o que quiser. Esta série venceu o prémio de Melhor Drama em três certames: International Emmy Awards, Prémios Fénix e Prémios Iris.");
+    System.out.println("");
+    System.out.println("Trailer: https://www.youtube.com/watch?v=iS5xXr-GOnM");
+  }
+}
